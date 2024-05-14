30f2b735215880327fb26161eec1c30c13b4aac6
mogimogiko <moemmmoe66@gmail.com>
2024-05-14 18:07:35 +0900

    なめらかにする

--- script.js
+++ script.js
@@ -33,8 +33,21 @@ $(function () {
 
   // ページ内リンクのスクロールをなめらかにする
   // ♯で始まるアンカーをクリックしたときの処理
-  $('.migi a[href^="#"]').on('click', function () {
-    var className =$('a').attr('migi');
-    console.log(className);
-  })
+  $('a[href^="#"]').on('click', function (e) {
+
+    // デフォルトのアクションを防止
+    e.preventDefault();
+
+    // クリックされたリンクのhref属性からアンカー名を取得
+    var id = $(this).attr('href');
+    
+    // 移動先のアンカーのページ上を取得
+    var target = $(id).offset().top;
+
+    // 滑らかにスクロールする
+    $('html, body').animate({scrollTop: target}, 600);
+  });
+
+  // sectionをスクロールしたらフェードインさせる
+  
 });
